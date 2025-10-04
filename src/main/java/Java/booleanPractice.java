afbf3075dbd90af5a787098341c5b13f0dc031a5
Siddhant Bhargava <amanbhargava145@gmail.com>
2025-10-04 21:50:04 +0530

    new class

---
+++ src/main/java/Java/booleanPractice.java
@@ -0,0 +1,18 @@
+package Java;
+
+public class booleanPractice {
+	
+	public static void main(String arr[]) {
+		
+		String s = "2";
+		String s2= " ";
+		
+		System.out.println(s.isBlank());
+		System.out.println(s2.isBlank());
+		
+		System.out.println(s.isEmpty());
+		System.out.println(s2.isEmpty());
+		
+	}
+
+}
